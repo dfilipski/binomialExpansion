01af44a99674559073a7176fdec6fbd491f283f3
dfilipski <danny.filipski@gmail.com>
2021-05-26 18:14:32 -0400

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+// import java.util.regex.Matcher;
+// import java.util.regex.Pattern;
+
+public class Driver
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        // "\(([0-9]*| -)[A-z]\+[0-9]*\)\^(1*[0-9])"
+        // Pattern p = Pattern.compile("\\(([0-9]*| -)[A-z]\\+[0-9]*\\)\\^(1*[0-9])");
+
+
+        System.out.println("Input a binomial in the form (ax+b)^n");
+        System.out.println("n must be an integer less than 13 and greater than -1");
+        System.out.println("x may be replaced with any letter");
+        String input = sc.nextLine();
+        // Matcher m = p.matcher(input);
+        // while (!m.find()) {
+        //     System.out.println("Input a binomial in the form (ax+b)^n");
+        //     System.out.println("n must be an integer less than 13 and greater than -1");
+        //     input = sc.nextLine();
+        // }
+        Expand e = new Expand(input);
+        System.out.println("\n" + e);
+    }
+}
---
+++ src/Expand.java
@@ -0,0 +1,85 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Expand {
+    private int a;
+    private int b;
+    private int n;
+    private char x;
+    private String output;
+
+    public Expand(String input) {
+        output = "";
+        a = getA(input);
+        x = getX(input);
+        b = getB(input);
+        n = getN(input);
+        expand();
+    }
+
+    private Integer getA(String input) {
+        int a;
+        Pattern p = Pattern.compile("[A-z]");
+        Matcher m = p.matcher(input);
+
+        if (!m.find()) {
+            return null;
+        }
+        int xIndex = m.start();
+        if (xIndex == 1) {
+            return 1;
+        } else if (xIndex == 2 && input.charAt(1) == '-') {
+            return -1;
+        } else {
+            a = Integer.parseInt(input.substring(1,xIndex));
+            return a;
+        }
+    }
+
+    private char getX(String input) {
+        Pattern p = Pattern.compile("[A-z]");
+        Matcher m = p.matcher(input);
+        if (!m.find()) {
+            return '~';
+        } else {
+            return input.charAt(m.start());
+        }
+    }
+
+    private int getB(String input) {
+        int b = Integer.parseInt(input.substring(input.indexOf("+") + 1, input.indexOf(")")));
+        return b;
+    }
+
+    private int getN(String input) {
+        int n = Integer.parseInt(input.substring(input.indexOf("^") + 1));
+        return n;
+    }
+
+    private void expand() {
+        for (int i = n; i >= 0; --i) {
+            output += binomCoef(n,i)*((int) Math.pow(a,i))*((int) Math.pow(b, (n-i)));
+            if (i == 0) {
+                output += " + ";
+            } else if (i == 1) {
+                output += x + " + ";
+            }else {
+                output += x +"^" + i + " + ";
+            }
+        }
+    }
+
+    private int binomCoef(int n, int k) {
+        return (factorial(n))/ (factorial(k) * factorial(n-k));
+    }
+
+    private int factorial (int n) {
+        int[] fact = {1, 1, 2, 6, 24, 120, 720, 5040, 40320,
+                362880, 3628800, 39916800, 479001600};
+        return fact[n];
+    }
+
+    public String toString() {
+        return output.substring(0, output.length() - 2);
+    }
+}
